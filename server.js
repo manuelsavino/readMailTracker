c32a1cc945f1dd3a59f68ffcc5a6ec616e9a03ec
manuelsavino <manuelsavino@gmail.com>
2019-01-22 13:45:06 -0500

    add / route

--- server.js
+++ server.js
@@ -5,11 +5,17 @@ app.use('/public', express.static(path.join(__dirname, 'public')))
 
 let PORT = process.env.port || 3000;
 
+app.get("/", (req, res)=>{
+    res.send("hello")
+})
+
 app.get("/:sender/:subject", (req, res)=> {
     const {sender, subject} =req.params;
     console.log(`${sender} ${subject}`)
     res.sendFile(path.join(__dirname, "./public", "pix.png"));
 })
+
+
 
 
 app.listen(PORT, ()=> {
