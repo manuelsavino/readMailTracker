3a47c702feb7b60ef7a28aad418f926d28e29f21
manuelsavino <manuelsavino@gmail.com>
2019-01-24 17:58:13 -0500

    change to record base

--- models/record.js
+++ models/record.js
@@ -2,13 +2,19 @@ const mongoose = require('mongoose'),
       Schema = mongoose.Schema;
 
 let recordSchema = new Schema({
-    to: String,
-    subject: String,
-    date: {
-        type: Date,
-        default: Date.now
-    }
-})
+  to: String,
+  from: String,
+  subject: String,
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  accessed: [
+      {
+        date: Date
+      }
+    ]
+});
 
 let Record = mongoose.model("Record", recordSchema)
 module.exports = Record;
--- server.js
+++ server.js
@@ -1,31 +1,43 @@
 require("dotenv").config();
-const express = require('express'),
-      app = express(),
-      path = require('path'),
-      mongoose = require('mongoose'),
-      record = require('./models/record'),
-      nodemailer = require('nodemailer'),
-      bodyParser = require('body-parser')
-
-
+const express = require("express"),
+  app = express(),
+  path = require("path"),
+  mongoose = require("mongoose"),
+  record = require("./models/record"),
+  nodemailer = require("nodemailer"),
+  bodyParser = require("body-parser"),
+  PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/public', express.static(path.join(__dirname, 'public')))
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 mongoose.connect(
-    process.env.MONGODB_URI || `mongodb://manuel:Miami1111@ds163764.mlab.com:63764/emailtracker`,
+  process.env.MONGODB_URI ||
+    `mongodb://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@ds163764.mlab.com:63764/emailtracker`,
   { useNewUrlParser: true }
 );
 
-let PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 
-app.get("/", (req, res)=>{
-    res.send("hello")
+app.post("/record", (req, res)=> {
+    const {from, to, subject} = req.body
+    const newRecord = { to, from ,subject };
+    record.create(newRecord, (err, doc) => {
+      if (err) {
+          console.log(err);
+      }
+      else{
+          res.json(doc);
+      }
+      
+    });
+
 })
 
-app.get("/sendMail", (req, res)=>{
-
-    async function main() {
+app.get("/sendMail", (req, res) => {
+  async function main() {
     let transporter = nodemailer.createTransport({
       host: "smtp.sendgrid.net",
       port: 587,
@@ -35,62 +47,33 @@ app.get("/sendMail", (req, res)=>{
         pass: process.env.PASSWORD // generated ethereal password
       }
     });
-    let mailOptions = { from: '"Ramiro Acosta" <noreply@manuelsavino.com>', to: "manuelsavino@gmail.com", subject: "Days Off", text: "Hello world?", html: "<p>Hello, Feel free to take the rest of the week off.</p>" }; // sender address // list of receivers // Subject line // plain text body // html body
-    let info = await transporter.sendMail(mailOptions)
+    let mailOptions = {
+      from: '"Ramiro Acosta" <noreply@manuelsavino.com>',
+      to: "manuelsavino@gmail.com",
+      subject: "Days Off",
+      text: "Hello world?",
+      html: "<p>Hello, Feel free to take the rest of the week off.</p>"
+    }; // sender address // list of receivers // Subject line // plain text body // html body
+    let info = await transporter.sendMail(mailOptions);
     console.log("Message sent: %s", info.messageId);
-    } 
+  }
 
-    main().catch(console.error);
+  main().catch(console.error);
 
-    res.send("ok")
+  res.send("ok");
 });
 
-app.post("/email", (req, res) => {
-    const {name, email, subject, message} = req.body;
-
-    async function main() {
-        let transporter = nodemailer.createTransport({
-            host: "smtp.sendgrid.net",
-            port: 587,
-            secure: false, // true for 465, false for other ports
-            auth: {
-                user: "apikey", // generated ethereal user
-                pass: process.env.PASSWORD // generated ethereal password
-            }
-        });
-        let mailOptions = { from: '"Form Submission Bot" <noreply@manuelsavino.com>', 
-        to: "manuelsavino@gmail.com", 
-        subject: "New Form Submission", 
-        text: "Form Submission", 
-        html: `
-        From: ${name} <br />
-        Email: ${email} <br />
-        Subject: ${subject} <br />
-        
-        Message: <p>${message}</p>` }; 
-        let info = await transporter.sendMail(mailOptions)
-        console.log("Message sent: %s", info.messageId);
+app.get("/track/:to/:subject", (req, res) => {
+  const { to, subject } = req.params;
+  const newRecord = { to, subject };
+  record.create(newRecord, (err, doc) => {
+    if (!err) {
+      console.log(doc);
     }
-
-    main().catch(console.error);
-
-    res.send("ok")
+  });
+  res.sendFile(path.join(__dirname, "./public", "pix.png"));
 });
 
-app.get("/:to/:subject", (req, res)=> {
-    const {to, subject} =req.params;
-    const newRecord = {to, subject}
-    record.create( newRecord, (err, doc)=>{
-        if(!err){
-        console.log(doc)
-        }
-    })
-    res.sendFile(path.join(__dirname, "./public", "pix.png"));
-})
-
-
-
-
-app.listen(PORT, ()=> {
-    console.log(`listening on ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
